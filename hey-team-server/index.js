dfe8d6d0caf401b2378d09ad812ce0736d952fd9
Wesley Harvey <pinecone062@gmail.com>
2018-05-04 15:27:14 -0700

    Added dotenv and config; added static server;
    .gitignore: added .env;
    config/keys.js, dev.js, prod.js: added basic config setup;
    hey-team-server/index.js: added static server;
                              changed server to start script for Heroku

--- hey-team-server/index.js
+++ hey-team-server/index.js
@@ -1,15 +1,23 @@
 /**
    slack-bot/hey-team-server/index.js
    ==================================
+   Project: Hey-Team Slack App
+   Created: 2018-05-04
+   Version: 0.1.0
+   About:   Main server file
+   Notes:   
+   -----------------------------------
  */
+
+require('dotenv').config();
+
+const KEYS = require('../config/keys');
+const PORT = process.env.Port || process.env.DEV_PORT;
 
 const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const PORT = process.env.Port || 5000;
-
 
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
@@ -19,3 +27,13 @@ app.get('/api', (req, res) => {
 app.listen(PORT, () => {
   console.error(`process ${process.pid}: listening on port ${PORT}`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('hey-team-frontend/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+	res.sendFile(path.resolve(
+	    __dirname, 'hey-team-frontend', 'build', 'index.html'
+	));
+    });
+}
