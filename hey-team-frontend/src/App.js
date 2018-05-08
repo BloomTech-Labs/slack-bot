9bcf52be5b39c1189149e917fa58d06f40ba496a
manishavi <manishalal972@gmail.com>
2018-05-08 17:10:31 -0500

    importing routes and commented config for now

--- hey-team-frontend/src/App.js
+++ hey-team-frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
 import About from './components/About';
 import LandingPage from "./components/LandingPage";
 import Billing from "./components/Billing";
@@ -15,17 +17,19 @@ import Navigation from "./components/Navigation";
 // import logo from './logo.svg';
 import './App.css';
 
-import config from './.config';
-console.log('CONFIG=>', config);
+// import config from './.config';
+// console.log('CONFIG=>', config);
 
 class App extends Component {
   render() {
     return (
       <div>
         <div className="App">
+          <Navigation />
           <Route path='/' exact component={Homepage} />
           <Route path='/about' component={About} />
-          <Navigation />
+          <Route path='/signin' component={LogIn} />
+          <Route path='/signup' component={SignUp} />
         </div>
       </div>
     );
