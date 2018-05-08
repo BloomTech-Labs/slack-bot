06aed1df142464bbfc9ffc48169eab7d77b1b429
Wesley Harvey <pinecone062@gmail.com>
2018-05-08 12:29:27 -0700

    Authentication basically works at this point

--- hey-team-frontend/src/App.js
+++ hey-team-frontend/src/App.js
@@ -3,13 +3,67 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-import { Security, ImplicitCallback } from '@okta/okta-react';
+import { Security, ImplicitCallback, withAuth } from '@okta/okta-react';
 
 const config = {
   issuer: 'https://dev-262012.oktapreview.com/oauth2/default',
   redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oaexy0vsuGGEK8vi0h7'
+  client_id: '0oaey1jb0ehljxYdI0h7'
 }
+
+const Home = withAuth(class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false,
+      userinfo: 'no user'
+    }
+    this.login = this.login.bind(this);
+    this.logout= this.logout.bind(this);
+    this.checkAuthentication = this.checkAuthentication.bind(this);
+  }
+
+  async checkAuthentication() {
+    console.log('checking authentication...');
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated) {
+      const userinfo = await this.props.auth.getUser();
+      this.setState(userinfo);
+    }
+  }
+
+  async componentDidMount() {
+    console.log('componentDidMount...');
+    this.checkAuthentication()
+      .then(state => console.log(state))
+      .catch(err => console.error(err));
+  }
+
+  // async componentDidUpdate() {
+  //   console.log('componentDidUpdate...');
+  //   this.checkAuthentication()
+  //     .then(state => console.log(state))
+  //     .catch(err => console.error(err));
+  // }
+
+  async login() {
+    console.log('logging in...');
+    this.props.auth.login('/');
+  }
+  async logout() {
+    console.log('logging out...');
+    this.props.auth.logout('/');
+  }
+
+  render () {
+    return (
+      <div>
+	<button onClick={this.login}>Login</button>
+	<button onClick={this.logout}>Logout</button>
+      </div>
+    )
+  }
+});
 
 class App extends Component {
   render() {
@@ -28,6 +82,8 @@ class App extends Component {
               This will be HeyTeam Slack App
             </p>
 	  </div>
+	  <Route path='/' exact component={Home} />
+	  <Route path='/implicit/callback' component={ImplicitCallback}/>
 	</Security>
       </Router>
     );
