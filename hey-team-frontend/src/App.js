bc45a73d86343dcfe75a7dbf5cc32afeb6028582
Wesley Harvey <pinecone062@gmail.com>
2018-05-08 08:14:53 -0700

    Added Security component to App.js

--- hey-team-frontend/src/App.js
+++ hey-team-frontend/src/App.js
@@ -15,15 +15,20 @@ class App extends Component {
   render() {
     return (
       <Router>
-	<div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to HeyTeam Slack App</h1>
-          </header>
-          <p className="App-intro">
-            This will be HeyTeam Slack App
-          </p>
-	</div>
+	<Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
+        >
+	  <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to HeyTeam Slack App</h1>
+            </header>
+            <p className="App-intro">
+              This will be HeyTeam Slack App
+            </p>
+	  </div>
+	</Security>
       </Router>
     );
   }
