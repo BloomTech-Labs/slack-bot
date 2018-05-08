1a7a005148d5cf352199a68406f05b90645bc195
Wesley Harvey <pinecone062@gmail.com>
2018-05-08 08:12:58 -0700

    Added Router to App.js

--- hey-team-frontend/src/App.js
+++ hey-team-frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,15 +14,17 @@ const config = {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to HeyTeam Slack App</h1>
-        </header>
-        <p className="App-intro">
-          This will be HeyTeam Slack App
-        </p>
-      </div>
+      <Router>
+	<div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to HeyTeam Slack App</h1>
+          </header>
+          <p className="App-intro">
+            This will be HeyTeam Slack App
+          </p>
+	</div>
+      </Router>
     );
   }
 }
