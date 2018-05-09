df5d6728e806016d74ddcd7b3988f2cbb60b5bbf
manishavi <manishalal972@gmail.com>
2018-05-09 17:06:05 -0500

    chaged index file for auth

--- hey-team-frontend/src/index.js
+++ hey-team-frontend/src/index.js
@@ -4,10 +4,25 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Security } from "@okta/okta-react";
+
+import config from "./app.config";
+
+function onAuthRequired({ history }) {
+  history.push("/login");
+}
 
 ReactDOM.render(
-<Router>
-  <App />
-</Router>
-  , document.getElementById('root'));
+  <Router>
+    <Security
+      issuer={config.issuer}
+      client_id={config.client_id}
+      redirect_uri={config.redirect_uri}
+      onAuthRequired={onAuthRequired}
+    >
+      <App />
+    </Security>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
