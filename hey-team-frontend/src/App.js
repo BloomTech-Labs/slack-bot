80421ef11aa491e244e23d45216ba764dd25f231
manishavi <manishalal972@gmail.com>
2018-05-09 17:06:53 -0500

    added routes

--- hey-team-frontend/src/App.js
+++ hey-team-frontend/src/App.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { SecureRoute, ImplicitCallback } from "@okta/okta-react";
 
-import { Route } from 'react-router-dom';
-import Homepage from './components/Homepage';
-import LogIn from "./components/LogIn";
-import SignUp from "./components/SignUp";
-import About from './components/About';
-import LandingPage from "./components/LandingPage";
-import Billing from "./components/Billing";
-import CreateConversation from "./components/CreateConversation";
-import ConversationDetail from "./components/ConversationDetail";
-import ConversationEdit from "./components/ConversationEdit";
-import ConversationList from "./components/ConversationList";
-import ConversationListEmp from "./components/ConversationListEmp";
-import Navigation from "./components/Navigation";
+import Navigation from "./components/shared/Navigation";
+import HomePage from "./components/home/HomePage";
+import RegistrationForm from "./components/auth/RegistrationForm";
+import config from "./app.config";
+import LoginPage from "./components/auth/LoginPage";
+import ProfilePage from "./components/auth/ProfilePage";
+import "./App.css";
 
-// import logo from './logo.svg';
-import './App.css';
-
-// import config from './.config';
-// console.log('CONFIG=>', config);
-
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div>
-        <div className="App">
-          <Navigation />
-          <Route path='/' exact component={Homepage} />
-          <Route path='/about' component={About} />
-          <Route path='/signin' component={LogIn} />
-          <Route path='/signup' component={SignUp} />
-        </div>
+      <div className="App">
+        <Navigation />
+        <main>
+          <Route path="/" exact component={HomePage} />
+          <Route
+            path="/login"
+            render={() => <LoginPage baseUrl={config.url} />}
+          />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+          <Route path="/register" component={RegistrationForm} />
+          <SecureRoute path="/profile" component={ProfilePage} />
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+
