acb8d2e2979c846d3df799974d574b0041fbdc1b
manishavi <manishalal972@gmail.com>
2018-05-15 18:17:32 -0500

    added div

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default class HomePage extends React.Component {
   render() {
-    return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>);
   }
 }
 
