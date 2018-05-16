b3404e69c1796fa860603f59b9a17a3ff02d6d2c
JCamp-12 <JChandlerCampbell@gmail.com>
2018-05-16 15:52:42 -0400

    carousel with not production ready gifs, billing page, buttons added

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -1,8 +1,26 @@
 import React from "react";
+import HomePageCarousel from '../Carousel';
+import { Button } from 'react-bootstrap'
 
 export default class HomePage extends React.Component {
   render() {
-    return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <HomePageCarousel />
+      <h3>
+        This is text between the buy button and the carousel. <br />
+        We will present our pitch for why you should click, <br />
+        and some basic details.  Any questions will be <br />
+        covered by a FAQ and our billing details, you can also <br />
+        send us an email for additional assistance.
+      </h3>
+      <Button bsStyle='success' bsSize="large">
+        Buy!!
+      </Button>
+      
+    </div>
+    );
   }
 }
 
