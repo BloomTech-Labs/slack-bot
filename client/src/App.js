97ae97a7d43209b5715d5869fecb4a5caadba5f2
Wesley Harvey <pinecone062@gmail.com>
2018-05-16 11:07:23 -0700

    Replaced App.js with new App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,39 @@
+/**
+   App.js
+   ====================================================
+   CREATED: 2018-05-16
+   VERSION 0.1.0
+   TEAM: Jason Campbell, Manisha Lal, Wesley Harvey
+   ABOUT: Main Application Component
+   NOTES:
+   ----------------------------------------------------
+ */
+
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+import Navigation from './components/shared/Navigation';
+import HomePage from './components/home/HomePage';
+import RegistrationForm from './components/auth/RegistrationForm';
+import config from './client.config';
+import LoginPage from './components/auth/LoginPage';
+import ProfilePage from './components/auth/ProfilePage';
 import './App.css';
 
-import { Security, ImplicitCallback, withAuth } from '@okta/okta-react';
-
-const config = {
-  issuer: 'https://dev-262012.oktapreview.com/oauth2/default',
-  redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oaey1jb0ehljxYdI0h7'
-}
-
-const Home = withAuth(class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      authenticated: false,
-      userinfo: 'no user'
-    }
-    this.login = this.login.bind(this);
-    this.logout= this.logout.bind(this);
-    this.checkAuthentication = this.checkAuthentication.bind(this);
-  }
-
-  async checkAuthentication() {
-    console.log('checking authentication...');
-    const authenticated = await this.props.auth.isAuthenticated();
-    if (authenticated) {
-      const userinfo = await this.props.auth.getUser();
-      this.setState(userinfo);
-    }
-  }
-
-  async componentDidMount() {
-    console.log('componentDidMount...');
-    this.checkAuthentication()
-      .then(state => console.log(state))
-      .catch(err => console.error(err));
-  }
-
-  // async componentDidUpdate() {
-  //   console.log('componentDidUpdate...');
-  //   this.checkAuthentication()
-  //     .then(state => console.log(state))
-  //     .catch(err => console.error(err));
-  // }
-
-  async login() {
-    console.log('logging in...');
-    this.props.auth.login('/');
-  }
-  async logout() {
-    console.log('logging out...');
-    this.props.auth.logout('/');
-  }
-
-  render () {
-    return (
-      <div>
-	<button onClick={this.login}>Login</button>
-	<button onClick={this.logout}>Logout</button>
-      </div>
-    )
-  }
-});
-
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <Router>
-	<Security issuer={config.issuer}
-                  client_id={config.client_id}
-                  redirect_uri={config.redirect_uri}
-        >
-	  <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to HeyTeam Slack App</h1>
-            </header>
-            <p className="App-intro">
-              This is HeyTeam Slack App
-            </p>
-	  </div>
-	  <Route path='/' exact component={Home} />
-	  <Route path='/implicit/callback' component={ImplicitCallback}/>
-	</Security>
-      </Router>
+      <div className="App">
+        <Navigation />
+        <main>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/login" render={() => <LoginPage baseUrl={config.url} />} />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+          <Route path="/register" component={RegistrationForm} />
+          <SecureRoute path="/profile" component={ProfilePage} />
+        </main>
+      </div>
     );
   }
 }
-
-export default App;
