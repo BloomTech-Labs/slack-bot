a566c4ce61c0d831690ec8cadf5b5161db6c36b9
Wesley Harvey <pinecone062@gmail.com>
2018-05-16 09:45:49 -0700

    updated config/prod.js variables

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,8 @@
    ====================================
    Project: Hey-Team Slack App
    Created: 2018-05-04
-   Version: 0.2.0
+   Updated: 2018-05-16
+   Version: 0.3.0
    Team: Jason Campbell, Manisha Lal,
          Christy Crites, Wesley Harvey
    About: Loads development environment
@@ -15,6 +16,6 @@
 module.exports = {
   client_id: process.env.OKTA_CLIENT_ID,
   issuer: `${process.env.OKTA_ORG_URI}/oauth2/default`,
-  redirect_uri: `https://${process.env.PROD_SERVER_URL}:${process.env.PORT}/authorization-code/callback`
+  redirect_uri: `https://${process.env.PROD_SERVER_URI}:${process.env.PORT}/authorization-code/callback`,
   scope: process.env.OKTA_SCOPE,
 }
