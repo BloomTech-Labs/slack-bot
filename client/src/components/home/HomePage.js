139b0777a6a0b98f12890a3c40251b2ff7b89a34
manishavi <manishalal972@gmail.com>
2018-06-19 14:01:31 -0500

    formatting

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -16,7 +16,8 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div>
-	<h1>Home Page</h1>
-      </div>);
+        <h1>HomePage</h1>
+      </div>
+    );
   }
 }
